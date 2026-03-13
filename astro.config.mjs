d5d2761b3cb347794824f30423a1194ecb994010
LaBayhu <rkrkrivan@gmail.com>
2026-03-13 21:58:37 +0800

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,5 +15,10 @@ export default defineConfig({
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      watch: {
+        ignored: ["**/storage/**"],
+      },
+    },
   },
 });
